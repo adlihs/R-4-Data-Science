c74add9031e28287fe051d1a209e1a0fd0d60843
Edmund Neil Quiros <edmund.neil@tigo.co.cr>
2018-03-01 11:59:31 -0600

    Ed3

--- scripts.R
+++ scripts.R
@@ -5,3 +5,41 @@ ipak('tidyverse')
 #MPG Data Frame
 mpg
    
+#Creating a ggplot
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y= hwy))
+
+#A graphing template
+#ggplot(data= <DATA>) +
+  #<GEOM_FUNCTION>(mapping= aes(x=DATA.VAR, y=DATA.VAR))
+
+
+#Exercises
+
+ggplot(data = mpg)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=hwy, y=cyl))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=class, y=drv))
+
+
+#Aesthetic mappings
+
+#Color
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, color = class))
+
+#Size
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, size = class))
+
+#Transparency
+ggplot(data=mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, alpha = class))
+
+#Shape
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, shape=class))
